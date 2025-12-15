8cab01a44cbc50e3053bcf126268098815afbfb0
ulkakulka5 <134429167+ulkakulka5@users.noreply.github.com>
2025-12-15 08:43:06 +0100

    Add event handlers for button clicks in AddPage

--- MauiApp1/AddPage.xaml.cs
+++ MauiApp1/AddPage.xaml.cs
@@ -1,9 +1,40 @@
+using System.Collections.ObjectModel;
+
 namespace MauiApp1;
 
 public partial class AddPage : ContentPage
 {
+  
     public AddPage()
     {
+
         InitializeComponent();
+       
+       
+    }
+
+    private void x250_Clicked(object sender, EventArgs e)
+    {
+        
+    }
+
+    private void x500_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void x180_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void custom_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private async void ok_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new MainPage());
     }
 }
