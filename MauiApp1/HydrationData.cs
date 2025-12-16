fcfb83f93b7726ae51128ed7b513e6a1f0677a6d
ulkakulka5 <134429167+ulkakulka5@users.noreply.github.com>
2025-12-16 12:17:06 +0100

    Add goal boolean to HydrationData

--- MauiApp1/HydrationData.cs
+++ MauiApp1/HydrationData.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,4 +10,5 @@ public static class HydrationData
 {
     public static int drunk_ml = 0;
     public static int goal_ml = 2000;
+    public static bool goal = false;
 }
